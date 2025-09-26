f4ee5e9a01381d2ee6f5ccbcc0dbe50266b32caa
heffzz <luca6854@gmail.com>
2025-09-26 13:51:08 +0200

    fix(syntax): rimuove righe di commento errate che causavano errore JSX in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -176,8 +176,7 @@ export default function Home() {
       </section>
 
       {/* Zona di Servizio */}
--      <section className="section-padding">
-+      <section className="section-padding bg-white">
+      <section className="section-padding bg-white">
         <div className="container-custom">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
             <div>
